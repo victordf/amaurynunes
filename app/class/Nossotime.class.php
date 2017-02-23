bf0e75ce9e88e7f5451e6572743554e4076a1c28
Victor Martins Machado <victormachado90@gmail.com>
2017-02-22 21:04:41 -0300

    Criação da rotina que salva o membro do time no banco de dados.

--- app/class/Nossotime.class.php
+++ app/class/Nossotime.class.php
@@ -10,6 +10,7 @@ namespace App;
 
 use Silex\Application;
 use Silex\Api\ControllerProviderInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -42,7 +43,64 @@ DML;
         });
 
         $this->con->post('/', function(Request $request) use($app){
+            $req  = $request->request->all();
+            $file = $request->files->get('file');
 
+            $nome      = $req['nome'];
+            $funcao    = $req['funcao'];
+            $resumo    = $req['resumo'];
+            $curriculo = $req['curriculo'];
+            $idiomas    = $req['idioma'];
+            $arquivo   = '';
+
+            if(!empty($file)){
+                $fotoNome = $file->getClientOriginalName();
+                $dir      = RAIZ . '/web/images/time';
+                $dirF     = __DIR__ . '/web/images/time';
+                $arquivo  = $dir . $fotoNome;
+                $arquivoF = $dirF . $fotoNome;
+
+                if(is_writable($dirF)){
+                    if(!move_uploaded_file($file->getPathname(), $arquivoF)){
+                        throw new Exception('Erro ao salvar o arquivo');
+                    }
+                } else {
+                    throw new Exception('Diretório inválido');
+                }
+            }
+
+            $sql = <<<DML
+                insert into nossotime (nome, funcao, resumo, curriculo, foto) values (
+                    '{$nome}',
+                    '{$funcao}',
+                    '{$resumo}',
+                    '{$curriculo}',
+                    '{$arquivo}'
+                );
+DML;
+            $app['db']->exec($sql);
+
+            $id = $app['db']->lastInsertId();
+
+            if(is_array($idiomas)){
+                foreach ($idiomas as $idioma) {
+                    $sql = <<<DML
+                        insert into idiomatime (ididioma, idtime) values (
+                            {$idioma},
+                            {$id}
+                        )
+DML;
+                    $app['db']->exec($sql);
+                }
+            }
+
+            $app['session']->set('ERRO', array(
+                'titulo' => 'Membro salvo com sucesso',
+                'msg' => '',
+                'tipo' => 'success'
+            ));
+
+            return new RedirectResponse(RAIZ."admin/nossotime/");
         });
 
         return $this->con;
